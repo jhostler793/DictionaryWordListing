558456e80fb63ccb67472ff95b380eb941587918
jhostler793 <jmh12@hood.edu>
2020-08-28 16:46:06 -0400

    Update

---
+++ src/DictionaryWordListing.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class DictionaryWordListing {
+    public static void main(String[] args) {
+ /*You are to a write a program which asks a user to enter 10 words.  Save these words in an array.  Using the bubble sort algorithm
+ (see below), sort the words in an ascending order. For the output, print out the words in the original order, in the ascending order,
+ and in the descending order. Finally, print an array where all letters have been changed to uppercase. Don't forget to write a pseudocode
+ or flowchart BEFORE writing your code.
+
+Your output should look something like this:
+
+Enter 10 words: kiwi pineapple apple banana orange grapes watermelon cherry strawberry melon
+
+Original Order:
+kiwi pineapple apple banana orange grapes watermelon cherry strawberry melon
+
+Alphabetical Order:
+apple banana cherry grapes kiwi melon orange pineapple strawberry watermelon
+
+Reversed Alphabetical Order:
+watermelon strawberry pineapple orange melon kiwi grapes cherry banana apple
+
+Upper case array:
+APPLE BANANA CHERRY GRAPES KIWI MELON ORANGE PINEAPPLE STRAWBERRY WATERMELON
+
+Need scanner -- value to where only 10 words can be entered -- need array added -- loop for alphabetical, reverse, upper case
+line at bottom when complete -- need string for words */
+
+    Scanner scanner = new Scanner(System.in);
+    String[] words = new String[10];
+    int count = 0;
+        System.out.println("Enter 10 words: ");
+
+        for (int i = 0; i < 10; i++){
+            //system.out.print
+
+    // This is the point at which I deleted the rest of the broken code/for-loop. The failing
+    // grade does not reflect how benefical this course was.
+
+        }
+
+    }
+}
